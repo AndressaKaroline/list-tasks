f5ad44e65c85c5189d0a844732e4dce4eca1b655
brunabelo <anabruna28@gmail.com>
2019-10-26 20:42:06 -0300

    adicionado CRUD do provider

--- src/providers/task/task.ts
+++ src/providers/task/task.ts
@@ -6,4 +6,53 @@ import { DateTime } from 'ionic-angular';
 
 @Injectable()
 export class TaskProvider {
+
+  constructor(private storage: Storage, private datepipe: DatePipe) {
+  
+  }
+
+  public insert(task: Task){
+    let key = this.datepipe.transform(new Date, "ddMMyyyyHHmmss")
+    return this.save(key, task);
+  }
+
+  public update(key: string, task: Task){
+    return this.save(key, task);
+  }
+
+  private save(key: string, task: Task){
+    return this.storage.set(key, task)
+  }
+
+  public remove(key: string){
+    return this.storage.remove(key);
+  }
+
+  public getAll(){
+    let tasks: TaskList[] = [];
+    this.storage.forEach((value: Task, key: string, itrationNumber: Number) => {
+      let task = new TaskList(); 
+      task.key = key;
+      task.task = value;
+      tasks.push(task);
+    })
+    .then(() => {
+      return Promise.resolve(tasks);
+    })
+    .catch((error) => {
+      return Promise.reject(error);
+    })
+  }
+}
+
+export class Task{
+  //user: Usuario
+  name: String;
+  description: String;
+  date: DateTime;
+}
+
+export class TaskList {
+  key: string;
+  task: Task;
 }
